68a84275c7e1c164e39ac8bb82ca38b44ec267b4
unknown <Dmitry.jabkevich@gmail.com>
2020-04-23 23:48:50 +0300

    готов

--- main.cpp
+++ main.cpp
@@ -3,24 +3,53 @@
 #include <math.h>
 #include <conio.h>
 #include <fstream>
+#include <vector>
+#include <algorithm>
+#include <string>
 using  namespace std;
 
 class funcs {
     int height;
+    int heightP=0;
+    int heightN=0;
     int column;
-    double x0;
-    double x1;
-    double step;
-    double *arrX;
-    double *arrY;
-    char **arr;
+    vector<double> arrX;
+    vector<double> arrYPositive;
+    vector<double>arrYNegative ;
+    vector<double> arrXYP;
+    vector<double>arrXYN ;
     int min;
     int max;
+
     double MIN;
     double MAX;
-    double *arrYTemp;
+    vector<double> arrYTemp;
+
+    void clear(){
+        MIN =0;
+        MAX =0;
+        min =0;
+        max =0;
+        heightP=0;
+        heightN=0;
+        arrYPositive.clear();
+        arrYNegative.clear();
+        arrXYP.clear();
+        arrXYN.clear();
+        arrYPositive.resize(this->column);
+        arrYNegative.resize(this->column);
+        arrXYP.resize(this->column);
+        arrXYN.resize(this->column);
+        arrYTemp.resize(this->column);
+    }
 
     void MaxAndMin(int &min, int &max, double &MIN, double &MAX, double Y, int i){
+            if (Y > 0) {
+                heightP++;
+            } else if (Y < 0) {
+                heightN++;
+            }
+
         if(i==0) {
             MAX = Y;
             MIN = Y;
@@ -35,89 +64,176 @@ class funcs {
         }
     }
 
-    void deformationY(double * arrYTemp, int MAX){
-        for(int i=0; i<column; i++){
-            arrY[i] = round(arrYTemp[i] * (height-1)/MAX);
+    void deformationY( vector<double> arrYTemp, int MAX){
+        while(heightN+heightP<height){
+            if(heightN!=0){
+                heightN++;
+            }
+            if(heightN+heightP==height) break;
+            if(heightP!=0) {
+                heightP++;
+            }
         }
+        while(heightN+heightP>height){
+            if(heightN>0){
+                heightN--;
+            }
+            if(heightN+heightP==height) break;
+            if(heightP>0) {
+                heightP--;
+            }
+        }
+
+        int k=0;
+        int j=0;
+        for(int i=0; i<column; i++){
+            if(arrYTemp[i]>=0) {arrYPositive[k] = floor(arrYTemp[i] * (heightP) / abs(MAX));while(arrYPositive[k]>heightP){arrYPositive[k]--;}arrXYP[k++] =i;  }
+            if(arrYTemp[i]<0) {arrYNegative[j] = floor(arrYTemp[i] * (heightN) / abs(MIN))*-1;while(arrYNegative[j] >heightN){arrYNegative[j]--;}arrXYN[j++] =i;}
+        }
+        arrXYP.resize(k);
+        arrYPositive.resize(k);
+        arrXYN.resize(j);
+        arrYNegative.resize(j);
     }
 public:
     funcs(int column, int height, double x0, double x1) {
         this->column = column+1;
         this->height = height+1;
-        this->x0 = x0;
-        this->x1 = x1;
 
         //шаг по x от x0 до x1
-        step = (x1 - x0) / column;
+        double step = (x1 - x0) / column;
         step = round(step * 10000) / 10000;
         //шаг
 
         //отрезок от x0 до x1
-        arrX = new double[ this->column];
+        arrX.resize(this->column);
         arrX[0] = x0;
         arrX[ this->column-1] = x1;
         //
 
         //массив с индексами в конечном графике
-        arrY = new double[column];
+        arrYPositive.resize(this->column);
+        arrYNegative.resize(this->column);
+
+        arrXYP.resize(this->column);
+        arrXYN.resize(this->column);
         //
 
         //массив с вычисленными точками по Y
-        arrYTemp = new double[column];
+        arrYTemp.resize(this->column);
         //
 
         for (int i = 1; i < this->column-1; i++) {
             arrX[i] = arrX[i - 1] + step;
         }
     }
-    ~funcs() {
-        delete[] arrX;
-        delete[] arrY;
-        delete[] arrYTemp;
+
+
+    void Test(vector<double> arr, string  str){
+        int size = arr.size();
+        cout<<str<<":\n";
+        for (int i = 0; i <arr.size(); i++) {
+            cout<<arr[i]<<" ";
+        }
+        cout<<"\n";
     }
 
-
-
-    void powerFunctionTwo() {
+    void rootFunctionTwo() {
+        clear();
         for (int i = 0; i < column; i++) {
-            arrYTemp[i] = arrX[i] * arrX[i];
+            arrYTemp[i] = arrX[i];
             MaxAndMin(min, max, MIN, MAX,  arrYTemp[i], i );
         }
+        cout<<"--------------------------\n";
+        Test(arrX, "arrX");
+        Test(arrYTemp,"arrYTemp");
         deformationY(arrYTemp, MAX);
+        Test(arrYPositive, "arrYPositive");
+        Test(arrYNegative, "arrYNegative");
+        Test(arrXYP, "arrXYP");
+        Test(arrXYN, "arrXYN");
+        cout<<"MAX: "<<MAX<<"\n";
+        cout<<"MIN: "<<MIN<<"\n";
+        cout<<"P: "<< heightP<<"\n";
+        cout<<"N: "<< heightN<<"\n";
+
+    };
+    void powerFunctionTwo() {
+        clear();
+        for (int i = 0; i < column; i++) {
+            arrYTemp[i] =arrX[i]*arrX[i];
+            MaxAndMin(min, max, MIN, MAX,  arrYTemp[i], i );
+        }
+        Test(arrX, "arrX");
+        Test(arrYTemp,"arrYTemp");
+        deformationY(arrYTemp, MAX);
+        Test(arrYPositive, "arrYPositive");
+        Test(arrYNegative, "arrYNegative");
+        Test(arrXYP, "arrXYP");
+        Test(arrXYN, "arrXYN");
+        cout<<"MAX: "<<MAX<<"\n";
+        cout<<"MIN: "<<MIN<<"\n";
+        cout<<"P: "<< heightP<<"\n";
+        cout<<"N: "<< heightN<<"\n";
     };
 
+    void Print(){
+        if(heightN>0){
+        heightN++;
+        }
+        if(heightP>0) {
+            heightP++;
+        }
+        vector<vector<char>> arrP(heightP,  vector<char>(column));
+        vector<vector<char>> arrN(heightN,  vector<char>(column));
 
-    void printXY() {
-        arr=new char*[height];
-        for(int i=0; i<height; i++){
-            arr[i] = new char[column];
+        for(int i=0; i<heightP; i++){
             for(int j=0; j<column; j++){
-                arr[i][j]= ' ';
+                arrP[i][j] ='+';
             }
         }
-        for(int i=0; i<column; i++){
-//            for(int j=0; j<=int(arrY[i]); j++){
-//                arr[j][i]='#';
-//            }
-            arr[int(arrY[i])][i]='#';
+        for(int i=0; i<heightN; i++){
+            for(int j=0; j<column; j++){
+                arrN[i][j] ='+';
+            }
+        }
+        for(int i=0; i<arrYPositive.size(); i++){
+            if(int(int(arrYPositive[i])==0)){
+                for(int j=0; j<column; j++) {
+                    arrP[int(arrYPositive[i])][j] = '-';
+                }
+            }
+        }
+        for(int i=0; i<arrYPositive.size(); i++){
+            arrP[int(arrYPositive[i])][int(arrXYP[i])]='#';
+        }
+        for(int i=0; i<arrYNegative.size(); i++) {
+            arrN[int(arrYNegative[i])][int(arrXYN[i])] = '#';
         }
         ofstream fout;
         fout.open("file.txt");
-        for(int i=height-1; i>=0; i--){
-            for(int j=column-1; j>=0; j--){
-                fout <<arr[i][j];
+        for(int i=heightP-1; i>=0; i--){
+            for(int j=0; j<column; j++){
+                fout <<arrP[i][j];
+            }
+            fout<<"\n";
+        }
+        for(int i=1; i<heightN; i++){
+            for(int j=0; j<column; j++){
+                fout <<arrN[i][j];
             }
             fout<<"\n";
         }
         fout.close();
     }
+
 };
 
 int main() {
-    funcs a(10, 20, -5, 5);
+    funcs a(10, 10, -5, 5);
     a.powerFunctionTwo();
-    a.printXY();
-
+    a.rootFunctionTwo();
+    a.Print();
     return 0;
 }
 
